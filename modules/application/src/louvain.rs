2ea8b943b5d4f8481c29640f4ae6bef5e52ad270
Splines <dominic-plein@gmx.de>
2023-07-25 23:35:54 +0200

    Add/Improve docstrings for Louvain

--- modules/application/src/louvain.rs
+++ modules/application/src/louvain.rs
@@ -20,23 +20,27 @@ impl<'a> Louvain<'a> {
 
     /// Runs the Louvain algorithm on the input graph.
     ///
-    /// Returns the hierarchy of community assignments of the nodes.
+    /// Returns
+    /// - the hierarchy of community assignments of the nodes
+    /// - the modularity of each level
     pub fn run(&self) -> (CommunityAssignmentHierarchy, Vec<f64>) {
         let mut hierarchy = CommunityAssignmentHierarchy::new();
         let mut modularities: Vec<f64> = Vec::new();
 
         let mut is_first_run = true;
         let mut working_graph: Option<LouvainGraph> = Option::None;
+
         loop {
             let (is_improvement, mut next_graph, modularity) = if is_first_run {
                 is_first_run = false;
                 self.run_level(self.graph, &mut hierarchy)
             } else {
                 self.run_level(
-                    &working_graph.expect("Next graph must have been set by now"),
+                    &working_graph.expect("Working graph must have been set by now"),
                     &mut hierarchy,
                 )
             };
+
             next_graph.calc_degrees();
             working_graph = Some(next_graph);
             modularities.push(modularity);
@@ -49,6 +53,19 @@ impl<'a> Louvain<'a> {
         (hierarchy, modularities)
     }
 
+    /// Runs one level of the Louvain algorithm on the input graph.
+    ///
+    /// Returns
+    /// - whether there was an improvement in modularity in the current level
+    /// - the next graph
+    /// - the modularity of the current level.
+    ///
+    /// Typically, there are only a few (e.g. 5) passes, i.e. this function
+    /// is only run a few times, until there is no improvement in modularity
+    /// anymore. This, of course, heavily depends on the input graph, its
+    /// size and the underlying community structure.
+    ///
+    /// "Level" might also be called one Louvain "pass".
     pub fn run_level(
         &self,
         graph: &LouvainGraph,
