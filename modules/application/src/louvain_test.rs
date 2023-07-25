f70a4cc46d4710710d7f1a7c8b7a8fb54dad648e
Splines <dominic-plein@gmx.de>
2023-07-26 00:20:28 +0200

    Fix duplicated graph init code in test case

--- modules/application/src/louvain_test.rs
+++ modules/application/src/louvain_test.rs
@@ -6,18 +6,6 @@ use crate::louvain_graph::louvain_graph_samples as samples;
 fn louvain_first_test() {
     // Just a first dummy test
     let mut graph = samples::house_and_triangle_graph();
-    graph.insert_edge(0, 1, 1.0);
-    graph.insert_edge(0, 4, 1.0);
-    graph.insert_edge(4, 1, 1.0);
-    graph.insert_edge(1, 2, 1.0);
-    graph.insert_edge(2, 3, 1.0);
-    graph.insert_edge(3, 4, 1.0);
-    graph.insert_edge(4, 5, 1.0);
-    graph.insert_edge(5, 6, 1.0);
-    graph.insert_edge(5, 8, 1.0);
-    graph.insert_edge(6, 7, 1.0);
-    graph.insert_edge(6, 8, 1.0);
-    graph.insert_edge(7, 8, 1.0);
 
     let louvain = Louvain::new(&mut graph);
     let (hierarchy, modularities) = louvain.run();
